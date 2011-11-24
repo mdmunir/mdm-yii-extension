b349faa2ff5b567e8a6d1f79abe044433e133d51
misbahuldmunir@gmail.com <misbahuldmunir@gmail.com@c5e7b088-075f-896e-6c5e-9be8047a1b68>
2011-11-24 07:25:47 +0000

    CK Editor

---
+++ MdmCkEditor/ckeditor/config.js
@@ -0,0 +1,37 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+//    config.extraPlugins = 'equation';
+//    config.toolbar = 'Basic';
+//
+//    config.toolbar_Full =
+//    [
+//    ['Source','-','Save','NewPage','Preview','-','Templates'],
+//    ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
+//    ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+//    ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
+//    '/',
+//    ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+//    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote','CreateDiv'],
+//    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+//    ['BidiLtr', 'BidiRtl' ],
+//    ['Link','Unlink','Anchor'],
+//    ['equation','Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe'],
+//    '/',
+//    ['Styles','Format','Font','FontSize'],
+//    ['TextColor','BGColor'],
+//    ['Maximize', 'ShowBlocks','-','About']
+//    ];
+//
+//    config.toolbar_Basic =
+//    [
+//    ['equation', '-', 'Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink','-','About']
+//    ];
+};
